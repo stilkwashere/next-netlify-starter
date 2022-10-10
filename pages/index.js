5a1ba4ba59d314f217a26bcbae3f29265b5d26fa
stilkwashere <115491393+stilkwashere@users.noreply.github.com>
2022-10-10 21:45:06 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,23 @@
-<script>
-info="<!DOCTYPE html>
-<html>
-<meta charset=&#34;UTF-8&#34;>
-<meta name=&#34;viewport&#34; content=&#34;width=device-width, initial-scale=1&#34;>
-<link rel=&#34;stylesheet&#34; href=&#34;https://www.w3schools.com/w3css/3/w3.css&#34;>
-<body>
-<h1>YEAH SCIENCE MR.WHITE</h1>
-<a href=&#34;chracterscreen.html&#34; >select character</a> <br>
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
-<a href=&#34;https://www.youtube.com/watch?v=aYW_jt7HkY4&#34; target=&#34;_blank&#34;>cool saul fnf mod</a>  <br>
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-<a href=&#34;https://www.youtube.com/watch?v=jeM9yRJwKl8&#34; target=&#34;_blank&#34;>lose your sanity over this</a>  <br>
+      <main>
+        <Header title="YO MISTA WHITE" />
+        <p className="description">
+          COOL SITE FOR OBJECT SHOW PROTOTYPE</code>
+        </p>
+      </main>
 
-</body>
-</html>"
-
-document.write(info)
-</script>S
+      <Footer />
+    </div>
+  )
+}
